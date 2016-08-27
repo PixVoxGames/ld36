93cf0b6672003bd52082c60a74434db27b195520
PixelIndigo <m4s4n0bu@gmail.com>
2016-08-27 15:10:15 +0500

    Editor skeleton :skull:

--- core/src/com/pixvoxgames/ld36/Game.java
+++ core/src/com/pixvoxgames/ld36/Game.java
@@ -7,29 +7,20 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Game extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
 	@Override
 	public void create() {
         Loggers.graphics.info("OpenGL {}", Gdx.gl.glGetString(GL20.GL_VERSION));
         Loggers.graphics.info("we're running on {} ({})", Gdx.gl.glGetString(GL20.GL_RENDERER), Gdx.gl.glGetString(GL20.GL_VENDOR));
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render() {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
 	}
 	
 	@Override
 	public void dispose() {
-		batch.dispose();
-		img.dispose();
 	}
 }
